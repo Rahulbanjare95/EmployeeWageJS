3c2ac1b92baecd2857ff1a6e4727bf9d43990c9c
Rahulbanjare95 <rahul.banjare95@gmail.com>
2020-08-03 01:53:45 +0530

    Added: Code to calculate total wage of the employee for one day

--- employeeWageComputation.js
+++ employeeWageComputation.js
@@ -1,7 +1,13 @@
-	let isPresent = 1;
-	let checkPresentOrAbsent = Math.floor(Math.random() * 10) % 2;
-	if (checkPresentOrAbsent == 1)
-		console.log("Employee is present");
-	else
-		console.log("Employee is Absent");
+let isPresent = 1;
+let EMP_RATE_PER_HR = 20;
+let empHrs = 0;
+let empWage = 0;
 
+let checkPresentOrAbsent = Math.floor(Math.random() * 10) % 2;
+if (checkPresentOrAbsent == 1)
+	empHrs = 8;
+else
+	empHrs = 0;
+
+empWage = empHrs * EMP_RATE_PER_HR;
+console.log(empWage);
