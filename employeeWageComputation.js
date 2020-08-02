651bbc19775b0816861863c2cd298a99ed36b8a5
Rahulbanjare95 <rahul.banjare95@gmail.com>
2020-08-02 22:56:06 +0530

    Refactored : Code to follow indentations properly

--- employeeWageComputation.js
+++ employeeWageComputation.js
@@ -1,7 +1,7 @@
-let isPresent = 1;
-let checkPresentOrAbsent = 
-Math.floor(Math.random() * 10) % 2;
-if (checkPresentOrAbsent == 1)
-			console.log("Employee is present");
-else
-			console.log("Employee is Absent");
+	let isPresent = 1;
+	let checkPresentOrAbsent = Math.floor(Math.random() * 10) % 2;
+	if (checkPresentOrAbsent == 1)
+		console.log("Employee is present");
+	else
+		console.log("Employee is Absent");
+
